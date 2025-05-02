07b36387d896754b6ff7fd59a56f5b4cfca59bc7
NIU1706696 <149660994+NIU1706696@users.noreply.github.com>
2025-05-02 16:42:19 +0200

    Update and rename fitxa.h to Fitxa.h

---
+++ Fitxa.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "Moviment.h"
+#include "Posicio.h"
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+enum Tipus
+{
+    TIPUS_BUIT,
+    TIPUS_NORMAL,
+    TIPUS_DAMA
+};
+enum Color
+{
+    COLOR_BLANC,
+    COLOR_NEGRE
+};
+
+class Tauler;
+class Fitxa
+{
+  public:
+    Fitxa() : m_color(COLOR_BLANC), m_tipo(TIPUS_BUIT)
+    {
+    }
+    Fitxa(Color color, Tipus type) : m_color(color), m_tipo(type)
+    {
+    }
+    Fitxa(char type);
+    Color getCol() const
+    {
+        return m_color;
+    }
+    Tipus getTipo() const
+    {
+        return m_tipo;
+    }
+    void upgrade(Posicio pos);
+    vector<Moviment> &getMoviments(Posicio pos, Tauler *tauler);
+    friend stringstream &operator<<(stringstream &out, const Fitxa &f);
+
+  private:
+    Color m_color;
+    Tipus m_tipo;
+    vector<Moviment> m_moviments;
+
+  private:
+    void trobaKillsDama(vector<Posicio> &kills, vector<Posicio> &posicions, Posicio nodeActual,
+                        const Moviment &currentPath, Tauler *t) const;
+    void trobaKillsNormal(vector<Posicio> &kills, vector<Posicio> &posicions, Posicio nodeActual,
+                          const Moviment &currentPath, Tauler *t) const;
+    void trobaKills(Posicio nodeActual, vector<Posicio> &kills, vector<Posicio> &posicions, const Moviment &currentPath,
+                    Tauler *tauler) const;
+    void trobaSaltsDama(vector<Posicio> &salts, Posicio nodeActual, Tauler *tauler) const;
+    void trobaSaltsNormal(vector<Posicio> &salts, Posicio nodeActual, Tauler *tauler) const;
+    vector<Posicio> trobaSalts(Posicio nodeActual, Tauler *tauler) const;
+};
